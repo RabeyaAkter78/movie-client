be2a408b0f9bb6e7c941eaffd8bb199893ebe3a5
RabeyaAkter78 <rabeyariya251@gmail.com>
2024-04-29 06:24:00 +0600

    added header footer and banner with responsiveness

---
+++ src/Pages/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+
+const Banner = () => {
+    return (
+        <div className="carousel justify-between items-center">
+        <div className="carousel-item">
+          <img src= "https://m.media-amazon.com/images/M/MV5BYjEyNWY5OTYtZjY4Ny00N2MwLTljOTItMzU4YzIyMWViNDgyXkEyXkFqcGdeQXVyMTczODE3MjQ4._V1_QL75_UX190_CR0,28,190,281_.jpg" alt="Burger" />
+        </div> 
+        <div className="carousel-item">
+          <img src="https://m.media-amazon.com/images/M/MV5BYWUwMGFmODMtYmFhNy00YTJlLWFiZDMtZDMwM2Q3NGZkNTgxXkEyXkFqcGdeQXVyMTU4Mzg1OTU2._V1_QL75_UY281_CR4,0,190,281_.jpg" alt="Burger" />
+        </div> 
+        <div className="carousel-item">
+          <img src= "https://m.media-amazon.com/images/M/MV5BMjRmNjBkODctY2IzNC00NTE1LTk1NzItZTFlZDVkMDkwMTU2XkEyXkFqcGdeQXVyMTQ3Mzk2MDg4._V1_QL75_UY281_CR77,0,190,281_.jpg" alt="Burger" />
+        </div> 
+        <div className="carousel-item">
+          <img src="https://m.media-amazon.com/images/M/MV5BM2UwMjRmNDEtODkxMi00ODg3LTkxODQtZDJkYTNkMGQ5N2Y1XkEyXkFqcGdeQXVyMTU0ODI1NTA2._V1_QL75_UY281_CR3,0,190,281_.jpg" alt="Burger" />
+        </div> 
+        <div className="carousel-item">
+          <img src="https://m.media-amazon.com/images/M/MV5BMTUzZGNhMzgtMDBkOC00MjdhLWI1YjAtY2E2MTYzMTQzYTE5XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_QL75_UY281_CR11,0,190,281_.jpg" alt="Burger" />
+        </div> 
+        <div className="carousel-item">
+          <img src= "https://m.media-amazon.com/images/M/MV5BNmU2Nzc2ZmItMTc5ZS00ZTE5LWEyOTMtMzIwYjBkNTI3Mjg4XkEyXkFqcGdeQXVyMTUyNjIwMDEw._V1_QL75_UY281_CR19,0,190,281_.jpg" alt="Burger" />
+        </div> 
+        <div className="carousel-item">
+          <img src="https://m.media-amazon.com/images/M/MV5BYzVmMGNhNzAtMDZkMy00ZDRjLWE1OTItNzViMWZmOWNjMjI3XkEyXkFqcGdeQXVyMTUzNTgzNzM0._V1_QL75_UY281_CR17,0,190,281_.jpg" alt="Burger" />
+        </div>
+      </div>
+    );
+};
+
+export default Banner;
